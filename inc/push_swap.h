39f90d4f4c6fd9583048694338662490f7b742b0
harleynghx <harleynghx.edu@gmail.com>
2024-11-04 15:58:25 +0800

    .h

--- inc/push_swap.h
+++ inc/push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: huaydin <huaydin@student.42vienna.com>     +#+  +:+       +#+        */
+/*   By: hang <hang@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/25 13:25:59 by huaydin           #+#    #+#             */
-/*   Updated: 2023/08/30 19:20:04 by huaydin          ###   ########.fr       */
+/*   Updated: 2024/11/04 15:57:25 by hang             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ void		parse_numbers(t_stacks *s);
 void		create_index(t_stacks *s);
 void		initialize_stacks(int argc, char **argv, t_stacks *s);
 void		free_and_exit_with_message(t_stacks *s, char *msg);
-void		exit_if_sorted_or_has_duplicate(t_stacks *s, int i);
+void		sorted_or_duplicate(t_stacks *s, int i);
 int			is_array_sorted(t_stacks *s);
 int			ft_atol(const char *n, t_stacks *s);
 
